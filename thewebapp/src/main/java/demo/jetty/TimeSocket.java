ccc0606ea3e1610fb3c489bc112556bdf5ebc5a9
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2020-10-12 14:11:58 -0500

    Adding license / copyright enforcement.

--- thewebapp/src/main/java/demo/jetty/TimeSocket.java
+++ thewebapp/src/main/java/demo/jetty/TimeSocket.java
@@ -1,6 +1,6 @@
 //
 //  ========================================================================
-//  Copyright (c) 1995-2015 Mort Bay Consulting Pty. Ltd.
+//  Copyright (c) Mort Bay Consulting Pty Ltd and others.
 //  ------------------------------------------------------------------------
 //  All rights reserved. This program and the accompanying materials
 //  are made available under the terms of the Eclipse Public License v1.0
@@ -23,7 +23,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
-
 import javax.websocket.CloseReason;
 import javax.websocket.OnClose;
 import javax.websocket.OnOpen;
